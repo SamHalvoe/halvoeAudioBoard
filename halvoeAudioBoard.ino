6c365ccddd5ef746d37727930d54e0db74f54020
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-04-13 18:20:01 +0200

    WIP: DUMP

--- halvoeAudioBoard.ino
+++ halvoeAudioBoard.ino
@@ -4,17 +4,17 @@
 #include <AudioCodecs/CodecWAV.h>
 
 // I2C
-#define SDAPIN              21 // I2C Data,  Adafruit ESP32 S3 3, Sparkfun Thing Plus C 23
-#define SCLPIN              22 // I2C Clock, Adafruit ESP32 S3 4, Sparkfun Thing Plus C 22
-#define I2CSPEED        100000 // Clock Rate
-#define WM8960ADDR        0x1A // Address of WM8960 I2C port
+#define SDA_PIN              21 // I2C Data,  Adafruit ESP32 S3 3, Sparkfun Thing Plus C 23
+#define SCL_PIN              22 // I2C Clock, Adafruit ESP32 S3 4, Sparkfun Thing Plus C 22
+#define I2C_SPEED        100000 // Clock Rate
+#define WM8960_ADDR        0x1A // Address of WM8960 I2C port
 
 // I2S, your configuration for the WM8960 board
-#define MCLKPIN             -1 // Master Clock
-#define BCLKPIN             27 // Bit Clock
-#define WSPIN               26 // Word select
-#define DOPIN               25 // This is connected to DI on WM8960 (MISO)
-#define DIPIN               -1 // This is connected to DO on WM8960 (MOSI)
+#define MCLK_PIN             -1 // Master Clock
+#define BCLK_PIN             27 // Bit Clock
+#define WS_PIN               26 // Word select
+#define DO_PIN               25 // This is connected to DI on WM8960 (MISO)
+#define DI_PIN               -1 // This is connected to DO on WM8960 (MOSI)
 
 AudioInfo                     audio_info(44100, 2, 16);                // sampling rate, # channels, bit depth
 DriverPins                    my_pins;                                 // board pins
@@ -24,7 +24,6 @@ I2SCodecStream                i2s_out_stream(audio_board);             // i2s co
 /*const char *startFilePath="/";
 const char* ext="wav";
 AudioSourceSDFAT source(startFilePath, ext);
-//I2SCodecStream i2s_out_stream(GenericWM8960);
 WAVDecoder decoder;
 AudioPlayer player(source, i2s_out_stream, decoder);*/
 
@@ -42,15 +41,15 @@ void setup()
   Serial.println("---- SETUP BEGIN ----");
 
   // setup i2c and i2s pins
-  my_pins.addI2C(PinFunction::CODEC, SCLPIN, SDAPIN, WM8960ADDR, I2CSPEED, Wire);
-  my_pins.addI2S(PinFunction::CODEC, MCLKPIN, BCLKPIN, WSPIN, DOPIN, DIPIN);
+  my_pins.addI2C(PinFunction::CODEC, SCL_PIN, SDA_PIN, WM8960_ADDR, I2C_SPEED, Wire);
+  my_pins.addI2S(PinFunction::CODEC, MCLK_PIN, BCLK_PIN, WS_PIN, DO_PIN, DI_PIN);
   my_pins.begin();
 
   audio_board.begin();
   
   // setup i2s
   auto i2s_config = i2s_out_stream.defaultConfig();
-  i2s_config.copyFrom(audio_info);  
+  i2s_config.copyFrom(audio_info);
   i2s_out_stream.begin(i2s_config);
 
   // set volume
