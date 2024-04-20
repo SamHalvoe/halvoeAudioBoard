d19a1d697ee243804ff46cc3a9dd5e263d6dd16d
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-04-20 21:12:58 +0200

    increased Serial baudrate which increased sound quality
    and did some other stuff

--- halvoeAudioBoard.ino
+++ halvoeAudioBoard.ino
@@ -16,10 +16,10 @@
 #define DO_PIN               25 // This is connected to DI on WM8960 (MISO)
 #define DI_PIN               -1 // This is connected to DO on WM8960 (MOSI)
 
-AudioInfo                     audio_info(44100, 2, 16);                // sampling rate, # channels, bit depth
-DriverPins                    my_pins;                                 // board pins
-AudioBoard                    audio_board(AudioDriverWM8960, my_pins); // audio board
-I2SCodecStream                i2s_out_stream(audio_board);             // i2s coded
+AudioInfo                     audio_info(44100, 2, 16);                   // sampling rate, # channels, bit depth
+DriverPins                    audio_pins;                                 // board pins
+AudioBoard                    audio_board(AudioDriverWM8960, audio_pins); // audio board
+I2SCodecStream                i2s_out_stream(audio_board);                // i2s coded
 
 const char* startFilePath="/";
 const char* ext="wav";
@@ -27,13 +27,9 @@ AudioSourceSDFAT source(startFilePath, ext);
 WAVDecoder decoder;
 AudioPlayer player(source, i2s_out_stream, decoder);
 
-/*SineWaveGenerator<int16_t> sineWave(32000);
-GeneratedSoundStream<int16_t> sound(sineWave);
-StreamCopy copier(i2s_out_stream, sound);*/
-
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Warning);
   delay(2000);
 
@@ -41,23 +37,26 @@ void setup()
   Serial.println("---- SETUP BEGIN ----");
 
   // setup i2c and i2s pins
-  my_pins.addI2C(PinFunction::CODEC, SCL_PIN, SDA_PIN, WM8960_ADDR, I2C_SPEED, Wire);
-  my_pins.addI2S(PinFunction::CODEC, MCLK_PIN, BCLK_PIN, WS_PIN, DO_PIN, DI_PIN);
-  my_pins.begin();
+  audio_pins.addI2C(PinFunction::CODEC, SCL_PIN, SDA_PIN, WM8960_ADDR, I2C_SPEED, Wire);
+  audio_pins.addI2S(PinFunction::CODEC, MCLK_PIN, BCLK_PIN, WS_PIN, DO_PIN, DI_PIN);
+  audio_pins.begin();
 
   //audio_board.begin();
   
   // setup i2s
   auto i2s_config = i2s_out_stream.defaultConfig();
   i2s_config.copyFrom(audio_info);
+  i2s_config.buffer_size = 2048;
   i2s_out_stream.begin(i2s_config);
 
   // set volume
-  i2s_out_stream.setVolume(0.625);
+  i2s_out_stream.setVolume(0.75);
 
   // setup player
+  player.setBufferSize(4096);
+  player.setSilenceOnInactive(true);
+  player.setAutoNext(false);
   player.begin();
-  //sineWave.begin(audio_info, N_B4);
 
   Serial.println("---- SETUP END ----");
 }
@@ -65,5 +64,4 @@ void setup()
 void loop()
 {
   player.copy();
-  //copier.copy();
 }
