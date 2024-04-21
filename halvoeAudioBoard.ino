f366c46b765f4b4356539abbf35954efe88b9d80
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-04-21 13:43:17 +0200

    rewrote playback from sd card
    replaced AudioPlayer & AudioSourceSDFAT with SdFat, File, EncodedAudioStream and StreamCopy

--- halvoeAudioBoard.ino
+++ halvoeAudioBoard.ino
@@ -1,7 +1,11 @@
+#include <SdFat.h>
 #include <AudioTools.h>
-#include <AudioLibs/AudioSourceSDFAT.h>
 #include <AudioLibs/I2SCodecStream.h>
 #include <AudioCodecs/CodecWAV.h>
+
+#define SD_CS_PIN 5
+#define SPI_CLOCK SD_SCK_MHZ(20)
+#define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_CLOCK)
 
 // I2C
 #define SDA_PIN              21 // I2C Data,  Adafruit ESP32 S3 3, Sparkfun Thing Plus C 23
@@ -16,16 +20,18 @@
 #define DO_PIN               25 // This is connected to DI on WM8960 (MISO)
 #define DI_PIN               -1 // This is connected to DO on WM8960 (MOSI)
 
-AudioInfo                     audio_info(44100, 2, 16);                   // sampling rate, # channels, bit depth
-DriverPins                    audio_pins;                                 // board pins
-AudioBoard                    audio_board(AudioDriverWM8960, audio_pins); // audio board
-I2SCodecStream                i2s_out_stream(audio_board);                // i2s coded
-
-const char* startFilePath="/";
-const char* ext="wav";
-AudioSourceSDFAT source(startFilePath, ext);
+AudioInfo audio_info(44100, 2, 16);                    // sampling rate, # channels, bit depth
+DriverPins audio_pins;                                 // board pins
+AudioBoard audio_board(AudioDriverWM8960, audio_pins); // audio board
+I2SCodecStream i2s_out_stream(audio_board);            // i2s coded
+const char* soundFilename = "ddd4416.wav";
+SdFat sdCard;
+File soundFile;
 WAVDecoder decoder;
-AudioPlayer player(source, i2s_out_stream, decoder);
+EncodedAudioStream encoded_out_stream(&i2s_out_stream, &decoder);
+StreamCopy audio_copier(encoded_out_stream, soundFile);
+
+bool isPlaybackActive = false;
 
 void setup()
 {
@@ -36,12 +42,22 @@ void setup()
   Serial.println("[Serial Ready]");
   Serial.println("---- SETUP BEGIN ----");
 
+  if (not sdCard.begin(SD_CONFIG))
+  {
+    sdCard.initErrorHalt(&Serial);
+  }
+
+  if (not soundFile.open(soundFilename, FILE_READ))
+  {
+    Serial.print("Could not open file: \"");
+    Serial.print(soundFilename);
+    Serial.println("\"");
+  }
+
   // setup i2c and i2s pins
   audio_pins.addI2C(PinFunction::CODEC, SCL_PIN, SDA_PIN, WM8960_ADDR, I2C_SPEED, Wire);
   audio_pins.addI2S(PinFunction::CODEC, MCLK_PIN, BCLK_PIN, WS_PIN, DO_PIN, DI_PIN);
   audio_pins.begin();
-
-  //audio_board.begin();
   
   // setup i2s
   auto i2s_config = i2s_out_stream.defaultConfig();
@@ -52,16 +68,30 @@ void setup()
   // set volume
   i2s_out_stream.setVolume(0.75);
 
-  // setup player
-  player.setBufferSize(4096);
-  player.setSilenceOnInactive(true);
-  player.setAutoNext(false);
-  player.begin();
+  // setup encoded stream
+  encoded_out_stream.begin(audio_info);
+
+  // setup audio copier
+  audio_copier.resize(4096);
+  audio_copier.begin();
+  isPlaybackActive = true;
 
   Serial.println("---- SETUP END ----");
 }
 
 void loop()
 {
-  player.copy();
+  if (isPlaybackActive)
+  {
+    size_t copiedBytesCount = audio_copier.copy();
+
+    if (copiedBytesCount == 0) // 0 bytes copied mean end of source
+    {
+      audio_copier.end();
+      soundFile.close();
+      i2s_out_stream.setMute(true);
+      isPlaybackActive = false;
+      Serial.println("playback finished");
+    }
+  }
 }
