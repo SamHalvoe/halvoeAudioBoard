ae2d884ec0a14260183ac5db25c105a3b34d4e00
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-11-03 20:27:51 +0100

    WIP: logging

--- halvoeLog.hpp
+++ halvoeLog.hpp
@@ -4,7 +4,7 @@
 #define DEBUGLOG_DEFAULT_FILE_LEVEL_INFO
 #define DEBUGLOG_ENABLE_FILE_LOGGER
 
-#include <SD.h> // this header is needed, because for file logging FILE_WRITE has to be defined and FS.h defines FILE_WRITE properly
+#include <SD.h> // this header is needed, because for file logging FILE_WRITE has to be defined and FS.h (included by SD.h) defines FILE_WRITE properly
 #include <DebugLog.h>
 #include <StreamUtils.hpp>
 #include <elapsedMillis.h>
